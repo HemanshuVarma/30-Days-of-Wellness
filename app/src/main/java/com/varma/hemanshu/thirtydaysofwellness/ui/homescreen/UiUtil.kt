62297cc8ece7004adf8303b8c51f5f8909681cb4
Hemanshu Varma <hemanshuvarma916@gmail.com>
2023-09-16 00:10:07 +0530

    Add: UI enhancements & animation

--- app/src/main/java/com/varma/hemanshu/thirtydaysofwellness/ui/homescreen/UiUtil.kt
+++ app/src/main/java/com/varma/hemanshu/thirtydaysofwellness/ui/homescreen/UiUtil.kt
@@ -1,11 +1,16 @@
 package com.varma.hemanshu.thirtydaysofwellness.ui.homescreen
 
+import androidx.compose.animation.animateContentSize
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.wrapContentHeight
 import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.KeyboardArrowDown
+import androidx.compose.material.icons.filled.KeyboardArrowUp
 import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
@@ -24,4 +29,14 @@ fun WellnessImage(thumbnailUrl: String) {
         loading = { CircularProgressIndicator(modifier = Modifier.wrapContentSize()) },
         contentDescription = "Wellness Tip Thumbnail"
     )
+}
+
+@Composable
+fun ItemIcon(isExpanded: Boolean, onClick: () -> Unit, modifier: Modifier = Modifier) {
+    IconButton(onClick = onClick, modifier = modifier.animateContentSize()) {
+        Icon(
+            imageVector = if (isExpanded) Icons.Default.KeyboardArrowUp else Icons.Default.KeyboardArrowDown,
+            contentDescription = if (isExpanded) "Hide description" else "Show description"
+        )
+    }
 }
--- app/src/main/java/com/varma/hemanshu/thirtydaysofwellness/ui/homescreen/WellnessItem.kt
+++ app/src/main/java/com/varma/hemanshu/thirtydaysofwellness/ui/homescreen/WellnessItem.kt
@@ -1,7 +1,12 @@
 package com.varma.hemanshu.thirtydaysofwellness.ui.homescreen
 
 import android.content.res.Configuration
+import androidx.compose.animation.animateContentSize
+import androidx.compose.animation.core.Spring
+import androidx.compose.animation.core.spring
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
@@ -11,6 +16,11 @@ import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
@@ -21,15 +31,36 @@ import com.varma.hemanshu.thirtydaysofwellness.ui.theme._30DaysOfWellnessTheme
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun WellnessItem(wellnessTip: WellnessTip, modifier: Modifier = Modifier) {
-    Card(modifier = modifier.fillMaxWidth(),
-        onClick = { /*TODO*/ }) {
-        Column(modifier = Modifier.padding(16.dp)) {
-            Text(
-                text = stringResource(id = wellnessTip.day),
-                style = MaterialTheme.typography.displaySmall
-            )
-            Spacer(modifier = Modifier.height(8.dp))
+fun WellnessItem(
+    wellnessTip: WellnessTip,
+    modifier: Modifier = Modifier
+) {
+    var isExpanded by remember { mutableStateOf(false) }
+    Card(
+        modifier = modifier.fillMaxWidth(),
+        onClick = { }
+    ) {
+        Column(
+            modifier = Modifier
+                .padding(16.dp)
+                .animateContentSize(
+                    animationSpec = spring(
+                        dampingRatio = Spring.DampingRatioMediumBouncy,
+                        stiffness = Spring.StiffnessMedium
+                    )
+                )
+        ) {
+            Row(
+                modifier = Modifier.fillMaxWidth(),
+                horizontalArrangement = Arrangement.SpaceBetween
+            ) {
+                Text(
+                    text = stringResource(id = wellnessTip.day),
+                    style = MaterialTheme.typography.displaySmall,
+                    modifier = Modifier.align(Alignment.CenterVertically)
+                )
+                ItemIcon(isExpanded = isExpanded, onClick = { isExpanded = !isExpanded })
+            }
             Text(
                 text = stringResource(id = wellnessTip.title),
                 style = MaterialTheme.typography.displayMedium
@@ -37,10 +68,12 @@ fun WellnessItem(wellnessTip: WellnessTip, modifier: Modifier = Modifier) {
             Spacer(modifier = Modifier.height(16.dp))
             WellnessImage(thumbnailUrl = wellnessTip.thumbnail)
             Spacer(modifier = Modifier.height(8.dp))
-            Text(
-                text = stringResource(id = wellnessTip.description),
-                style = MaterialTheme.typography.bodyLarge
-            )
+            if (isExpanded) {
+                Text(
+                    text = stringResource(id = wellnessTip.description),
+                    style = MaterialTheme.typography.bodyLarge
+                )
+            }
         }
     }
 }
@@ -49,15 +82,8 @@ fun WellnessItem(wellnessTip: WellnessTip, modifier: Modifier = Modifier) {
 @Composable
 fun WellnessItemLightPreview() {
 
-    val item = WellnessTip(
-        day = R.string.day_1,
-        title = R.string.title_d01,
-        thumbnail = "https://i.ytimg.com/vi/RLzC55ai0eo/hq720.jpg",
-        description = R.string.desc_d01
-    )
-
     _30DaysOfWellnessTheme {
-        WellnessItem(item)
+        WellnessItem(getDummyItem())
     }
 }
 
@@ -65,14 +91,14 @@ fun WellnessItemLightPreview() {
 @Composable
 fun WellnessItemDarkPreview() {
 
-    val item = WellnessTip(
-        day = R.string.day_1,
-        title = R.string.title_d01,
-        thumbnail = "https://i.ytimg.com/vi/RLzC55ai0eo/hq720.jpg",
-        description = R.string.desc_d01
-    )
-
     _30DaysOfWellnessTheme {
-        WellnessItem(item)
+        WellnessItem(getDummyItem())
     }
 }
+
+private fun getDummyItem() = WellnessTip(
+    day = R.string.day_1,
+    title = R.string.title_d01,
+    thumbnail = "https://i.ytimg.com/vi/RLzC55ai0eo/hq720.jpg",
+    description = R.string.desc_d01
+)
