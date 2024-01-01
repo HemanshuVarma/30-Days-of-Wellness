4f2ae3bb118f5bb4e604293e07b6b1fc7e775e7b
Hemanshu Varma <hemanshuvarma916@gmail.com>
2024-01-02 00:23:35 +0530

    Add: More Wellness info

--- app/src/main/java/com/varma/hemanshu/thirtydaysofwellness/repository/WellnessRepo.kt
+++ app/src/main/java/com/varma/hemanshu/thirtydaysofwellness/repository/WellnessRepo.kt
@@ -77,6 +77,114 @@ object WellnessRepo {
             title = R.string.title_d12,
             description = R.string.desc_d12,
             videoId = R.string.video_id_d12
+        ),
+        WellnessTip(
+            day = R.string.day_13,
+            title = R.string.title_d13,
+            description = R.string.desc_d13,
+            videoId = R.string.video_id_d13
+        ),
+        WellnessTip(
+            day = R.string.day_14,
+            title = R.string.title_d14,
+            description = R.string.desc_d14,
+            videoId = R.string.video_id_d14
+        ),
+        WellnessTip(
+            day = R.string.day_15,
+            title = R.string.title_d15,
+            description = R.string.desc_d15,
+            videoId = R.string.video_id_d15
+        ),
+        WellnessTip(
+            day = R.string.day_16,
+            title = R.string.title_d16,
+            description = R.string.desc_d16,
+            videoId = R.string.video_id_d16
+        ),
+        WellnessTip(
+            day = R.string.day_17,
+            title = R.string.title_d17,
+            description = R.string.desc_d17,
+            videoId = R.string.video_id_d17
+        ),
+        WellnessTip(
+            day = R.string.day_18,
+            title = R.string.title_d18,
+            description = R.string.desc_d18,
+            videoId = R.string.video_id_d18
+        ),
+        WellnessTip(
+            day = R.string.day_19,
+            title = R.string.title_d19,
+            description = R.string.desc_d19,
+            videoId = R.string.video_id_d19
+        ),
+        WellnessTip(
+            day = R.string.day_20,
+            title = R.string.title_d20,
+            description = R.string.desc_d20,
+            videoId = R.string.video_id_d20
+        ),
+        WellnessTip(
+            day = R.string.day_21,
+            title = R.string.title_d21,
+            description = R.string.desc_d21,
+            videoId = R.string.video_id_d21
+        ),
+        WellnessTip(
+            day = R.string.day_22,
+            title = R.string.title_d22,
+            description = R.string.desc_d22,
+            videoId = R.string.video_id_d22
+        ),
+        WellnessTip(
+            day = R.string.day_23,
+            title = R.string.title_d23,
+            description = R.string.desc_d23,
+            videoId = R.string.video_id_d23
+        ),
+        WellnessTip(
+            day = R.string.day_24,
+            title = R.string.title_d24,
+            description = R.string.desc_d24,
+            videoId = R.string.video_id_d24
+        ),
+        WellnessTip(
+            day = R.string.day_25,
+            title = R.string.title_d25,
+            description = R.string.desc_d25,
+            videoId = R.string.video_id_d25
+        ),
+        WellnessTip(
+            day = R.string.day_26,
+            title = R.string.title_d26,
+            description = R.string.desc_d26,
+            videoId = R.string.video_id_d26
+        ),
+        WellnessTip(
+            day = R.string.day_27,
+            title = R.string.title_d27,
+            description = R.string.desc_d27,
+            videoId = R.string.video_id_d27
+        ),
+        WellnessTip(
+            day = R.string.day_28,
+            title = R.string.title_d28,
+            description = R.string.desc_d28,
+            videoId = R.string.video_id_d28
+        ),
+        WellnessTip(
+            day = R.string.day_29,
+            title = R.string.title_d29,
+            description = R.string.desc_d29,
+            videoId = R.string.video_id_d29
+        ),
+        WellnessTip(
+            day = R.string.day_30,
+            title = R.string.title_d30,
+            description = R.string.desc_d30,
+            videoId = R.string.video_id_d30
         )
     )
 }
